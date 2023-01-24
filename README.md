9fc75997619771d11d03890d652289296f2d62c3
Harry_Desktop <hward.1508@gmail.com>
2023-01-24 19:47:04 +0800

    file write working and generating readme file

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Professional-README-Generator
-This is a professional readme generator
+# Titlw
+  [![MIT License license](https://img.shields.io/badge/License-MIT License-blue.svg)](undefined)
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  ## Description
+  fd
+  ## Installation
+  f
+  ## Usage
+  fg
+  ## Licenses
+    This project is covered under the MIT License license. To learn more about what this means, click the license button at the top.
+  ## Contributing
+  gfd
+  ## Tests
+  
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/undefined  
+  Email: undefined
+  ## Credits
+  undefined
--- index.js
+++ index.js
@@ -1,12 +1,72 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = ["Please Enter a Project Title", "Please enter a description of your project", "Please enter an Installation Guide", "Please provide usage information", "Please provide contribution guidelines", "Please provide test instructions", "Please choose your license","Please enter your GitHub Username","Please enter your email address"];
 
-// TODO: Create a function to write README file
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: "What is your email address?",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "Please enter the title of your project.",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must provide a title.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Please enter a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must provide a description.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'testing'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+];
+
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
         if (err) {
@@ -14,12 +74,24 @@ function writeToFile(fileName, data) {
     }
     console.log("Your README file has successfully created!");
     });
-}
+};
 
-// TODO: Create a function to initialize app
-function init() {
+async function init() {
     console.log("Starting app...");
-}
 
-// Function call to initialize app
+    try {
+        const responses = await inquirer.prompt(questions);
+        const generateContent = generateMarkdown(responses);
+
+        console.log("Responses Accepted \n\nGenerating content...\n");
+
+        await writeToFile('README.md', generateContent);
+        console.log("Successfully generated README\n");
+
+    } catch (err) {
+        console.error(err);
+    }
+
+};
+
 init();
